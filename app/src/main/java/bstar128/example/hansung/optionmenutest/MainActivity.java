f9d356fc805894e804a477fe810cce32bdf05e13
StarryPurp <starbloom128@gmail.com>
2017-05-23 19:59:02 +0900

    #4 Activity Menu

--- app/src/main/java/bstar128/example/hansung/optionmenutest/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/optionmenutest/MainActivity.java
@@ -1,16 +1,24 @@
 package bstar128.example.hansung.optionmenutest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button but;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linear=(LinearLayout)findViewById(R.id.linear);
+        but=(Button)findViewById(R.id.but_ok);
     }
 
     @Override
@@ -19,5 +27,30 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater mInflater=getMenuInflater();
         mInflater.inflate(R.menu.menu1,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.item_violet:
+                linear.setBackgroundColor(Color.MAGENTA);
+                return true;
+            case R.id.item_mint:
+                linear.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.item_but_rotate:
+                but.setRotation(but.getRotation()+45);
+                return true;
+            case R.id.item_but_size:
+                but.setScaleX(but.getScaleY()*1.5f);
+                return true;
+
+        }
+
+        return false;
     }
 }
